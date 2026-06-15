84415de199c4da52d2b0f1e107ebd466e3c71a74
Adhyaksa Margandatua Banjar Nahor <marbunadhy@gmail.com>
2026-06-15 23:37:33 +0700

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -106,4 +106,7 @@ def realtime():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5001))
+    )
