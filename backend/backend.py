c19a90186ebc8139ad89822732c133e8a004e8ee
Adhyaksa Margandatua Banjar Nahor <marbunadhy@gmail.com>
2026-06-16 00:18:33 +0700

    Change server port from 5001 to 8080

--- backend/backend.py
+++ backend/backend.py
@@ -108,5 +108,5 @@ def realtime():
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5001))
+        port=int(os.environ.get("PORT", 8080))
     )
